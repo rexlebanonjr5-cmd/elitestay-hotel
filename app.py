7a9cfc3d1d6e9bdf60568bc25fe11de91fa2cf26
Rex Agyei Boampong <rexlebanonjr5@gmail>
2026-03-13 05:13:27 +0000

    Fix Render port binding

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session 
 import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = "elitestay-secret-key"
@@ -117,4 +118,6 @@ def admin():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port =
+    int(os.environ.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
